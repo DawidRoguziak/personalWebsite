024994a99a356bf6ed13f9111db4b4ae49880ea8
DawidRoguziak <dawidroguziak@gmail.com>
2020-09-06 11:17:04 +0200

    modify svg in AboutMeSection

--- src/plugins/gsap.js
+++ src/plugins/gsap.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import gsap from 'gsap';
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
 
-
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, MorphSVGPlugin);
 
 Vue.prototype.$gsap = gsap;
