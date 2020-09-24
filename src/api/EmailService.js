87839297e21a6fb10331f7162e993b52a9a9ae3e
DawidRoguziak <dawidroguziak@gmail.com>
2020-09-24 20:50:40 +0200

    create script to send mail

--- src/api/EmailService.js
+++ src/api/EmailService.js
@@ -1,9 +1,41 @@
-const  express = require('express');
+const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
-router.post('/',  (req, res) => {
+router.post('/', (req, res) => {
 
-   res.send({'ala': 'test'});
+    const message = {
+        from: `Test mail <${process.env.MAIL_LOGIN}>`,
+        to: "",
+        subject: "",
+        text: "Test message",
+        html: "<p>Test message</p>"
+    };
+
+    const transporter = nodemailer.createTransport({
+        host: "SMTP.office365.com",
+        port: 587,
+        tls: {
+            // ciphers:'SSLv3'
+            rejectUnauthorized: false
+        },
+        secure: false,
+        auth: {
+            user: process.env.MAIL_LOGIN,
+            pass: process.env.MAIL_PASSWORD
+        }
+    });
+
+    transporter.sendMail(message,(error, info) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        return console.log('poszło')
+    });
+
+
+    res.send({'ala': 'test'});
 });
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,10 +7,12 @@ const  app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
 const port = 8001;
 const emailService = require('./api/EmailService');
-app.use('/api/email', emailService);
 
+app.use('/api/email', emailService);
 app.use(express.static(__dirname + '/public/'))
 app.get(/.*/, (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
