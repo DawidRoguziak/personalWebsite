d806f1d4be9c682e3493d94ceef08b802b99a233
DawidRoguziak <dawidroguziak@gmail.com>
2020-09-25 21:22:34 +0200

    add verification reCaptcha on backend

--- src/api/EmailService.js
+++ src/api/EmailService.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const EmailSender = require('./../services/EmailSender')
+const GoogleRecaptcha = require('./../services/GoogleRecaptcha')
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 
     const email = req.body.email || null;
     const title = req.body.title || null;
     const message = req.body.message || null;
+    const token = req.body.token || null;
 
-    if (!email || !title || !message) {
+    if (!email || !title || !message || !token) {
         res.send({success: false, error: null, notify: 'Missing data'});
+    }
+
+    const googleRecaptcha = new GoogleRecaptcha(token);
+    const isHuman = await googleRecaptcha.checkToken()
+    if(!isHuman) {
+        res.send({success: false, error: null, notify: 'You are not a human!!!'});
     }
 
     const emailSender = new EmailSender({
--- src/services/GoogleRecaptcha.js
+++ src/services/GoogleRecaptcha.js
@@ -1,3 +1,21 @@
+const fetch = require('isomorphic-fetch');
+
 class GoogleRecaptcha {
-     constructor() {}
+    constructor(token) {
+        this.token = token
+        this.link = this.googleCheckLink();
+    }
+
+    googleCheckLink() {
+        return `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_PRIVATE_KEY}&response=${this.token}`
+    }
+
+    checkToken() {
+        return fetch(this.link, {method: 'post'})
+            .then(response => response.json())
+            // .then(response => console.log(response.score );
+            .then(response => response.score >= 0.9);
+    }
 }
+
+module.exports = GoogleRecaptcha;
